c129805be26d4d687743a1c183bac03b5f39cf57
John Robert-Nicoud <john@6clicks.ch>
2016-12-06 10:45:55 +0100

    Suppression de fichiers

--- ftp-conf.js
+++
@@ -1,8 +0,0 @@
-var config = {
-      host:     'FTP HOST',
-      user:     'FTP USER',
-      pass:     'FTP PASSW',
-      port:  '21',      
-    dirdest: '/www/dir/dest/'
-};
-exports.config = config;
